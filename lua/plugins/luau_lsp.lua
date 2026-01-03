c7048a9915b7e20bf00e803785edfd7c741e1132
Bloodhundur <shahyan.soltani@gmail.com>
2026-01-02 21:33:39 -0500

    added roblox studio support

---
+++ lua/plugins/luau_lsp.lua
@@ -0,0 +1,19 @@
+return {
+	"lopi-py/luau-lsp.nvim",
+	ft = { "luau" },
+	opts = {
+		platform = {
+			type = "roblox",
+		},
+		types = {
+			roblox_security_level = "PluginSecurity",
+		},
+		sourcemap = {
+			enabled = true,
+			autogenerate = true, -- automatic generation when the server is initialized
+			rojo_project_file = "default.project.json",
+			sourcemap_file = "sourcemap.json",
+		},
+	},
+}
+
