56908ceb0353296468b2d579af782357ba626b9a
Bloodhundur <shahyan.soltani@gmail.com>
2026-02-09 18:08:18 -0500

    update of a bunch of stuff lol

--- init.lua
+++ init.lua
@@ -1,15 +1,22 @@
 require("config.lazy")
 -- ~/.config/nvim/init.lua
 vim.cmd[[colorscheme tokyonight-moon]]
+vim.diagnostic.config({ 
+	virtual_lines = {
+		current_line = true 
+	},
+	float = {
+	},
+})
+vim.opt.number = true
 vim.lsp.enable({
-'jdtls',
-'luals',
-'html',
-'rust_analyzer',
-'cssls',
-'typejavascript',
-'pyright',
-'clangd',
-'csharp_ls',
-'bashls'
+	'jdtls',
+	'luals',
+	'html',
+	'cssls',
+	'typejavascript',
+	'pyright',
+	'clangd',
+	'csharp_ls',
+	'bashls'
 })
--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -1,3 +1,4 @@
+--[[
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   event = "LspAttach",
@@ -10,3 +11,4 @@ return {
   end,
 }
 
+--]]
